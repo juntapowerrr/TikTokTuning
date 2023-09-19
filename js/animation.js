2c261baf229a40bec7193cbe398730f449a12732
Denys <80397210+juntapowerrr@users.noreply.github.com>
2023-09-19 14:40:04 +0300

    added_animations

---
+++ js/animation.js
@@ -0,0 +1,26 @@
+//Header
+ScrollReveal().reveal('.header', { delay: 200 });
+// ScrollReveal().reveal('.header__title', { delay: 200 });
+// ScrollReveal().reveal('#HeaderLink1', { delay: 250 });
+// ScrollReveal().reveal('#HeaderLink2', { delay: 300 });
+// ScrollReveal().reveal('#HeaderLink3', { delay: 350 });
+// ScrollReveal().reveal('#BtnCta1', { delay: 400 });
+
+//Main Section
+ScrollReveal().reveal('#MainImg', { delay: 100 });
+ScrollReveal().reveal('#BtnInfo1', { delay: 200 });
+ScrollReveal().reveal('#BtnInfo2', { delay: 250 });
+ScrollReveal().reveal('#BtnInfo3', { delay: 300 });
+ScrollReveal().reveal('#Title1', { delay: 200 });
+ScrollReveal().reveal('#Subtitle1', { delay: 250 });
+ScrollReveal().reveal('#BtnCta2', { delay: 275 });
+ScrollReveal().reveal('#BtnCta3', { delay: 300 });
+
+
+//About Section
+ScrollReveal().reveal('#Title2', { delay: 300 });
+ScrollReveal().reveal('#Card1', { delay: 100 });
+ScrollReveal().reveal('#Card2', { delay: 200 });
+ScrollReveal().reveal('#Card3', { delay: 300 });
+ScrollReveal().reveal('#Card4', { delay: 400 });
+ScrollReveal().reveal('#Card5', { delay: 500 });
