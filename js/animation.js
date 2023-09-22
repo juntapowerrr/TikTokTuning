9d30d5b658df83fa9f74b2047a71efc510f7bf22
Denys <80397210+juntapowerrr@users.noreply.github.com>
2023-09-22 16:50:48 +0300

    animation-add

--- js/animation.js
+++ js/animation.js
@@ -8,6 +8,7 @@ ScrollReveal().reveal('.header', { delay: 200 });
 
 //Main Section
 ScrollReveal().reveal('#MainImg', { delay: 100 });
+ScrollReveal().reveal('#MainImgMob', { delay: 100 });
 ScrollReveal().reveal('#BtnInfo1', { delay: 200 });
 ScrollReveal().reveal('#BtnInfo2', { delay: 250 });
 ScrollReveal().reveal('#BtnInfo3', { delay: 300 });
@@ -24,3 +25,19 @@ ScrollReveal().reveal('#Card2', { delay: 200 });
 ScrollReveal().reveal('#Card3', { delay: 300 });
 ScrollReveal().reveal('#Card4', { delay: 400 });
 ScrollReveal().reveal('#Card5', { delay: 500 });
+ScrollReveal().reveal('.quote-txt', { delay: 100 });
+ScrollReveal().reveal('.quote-author', { delay: 200 });
+ScrollReveal().reveal('.story', { delay: 300 });
+
+//Tuning Section
+ScrollReveal().reveal('#TuningTitle', { delay: 100 });
+ScrollReveal().reveal('.vector-1', { delay: 125 });
+ScrollReveal().reveal('#TuningCards', { delay: 150 });
+ScrollReveal().reveal('.vector-2', { delay: 175 });
+ScrollReveal().reveal('#TuningTitle2', { delay: 100 });
+ScrollReveal().reveal('#WhyTikTokItem1', { delay: 125 });
+ScrollReveal().reveal('#WhyTikTokItem2', { delay: 150 });
+ScrollReveal().reveal('#WhyTikTokItem3', { delay: 175 });
+
+//Program Section
+
