2be5b3a1dd9b43956a1be284df394f169cd205ac
Denys <80397210+juntapowerrr@users.noreply.github.com>
2023-09-22 22:51:34 +0300

    add-scroll(need-fix)

--- js/index.js
+++ js/index.js
@@ -1,4 +1,13 @@
 $(document).ready(function() {
+    $('.header__link, .btn').fixedHeaderScroll( {
+        headerSelector: 'header',
+        offset: 0,
+        offset: $('.header').offsetHeight,
+        smooth: true,
+        smoothDuration: 500,
+        smoothEasing: 'swing',
+    });
+
     $('.header__burger, .header__link').click(function(event) {
         $('.header__burger, .header__menu').toggleClass('active');
         $('body').toggleClass('lock');
